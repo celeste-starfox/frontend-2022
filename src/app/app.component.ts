5cfb89f2bed59d493dff65478e2e3d19cd3b55d6
adamWarder <adwarder@gmail.com>
2022-10-20 07:59:10 +0200

    Initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = `angularTest2022 ${yourName}`;
+]
+
